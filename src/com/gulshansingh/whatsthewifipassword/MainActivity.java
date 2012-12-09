f713cc86186969039ce65e2200845da39c053f70
Gulshan Singh <gulshan@umich.edu>
2012-12-09 14:34:43 -0500

    Refreshed display onResume instead of only in onCreate

--- src/com/gulshansingh/whatsthewifipassword/MainActivity.java
+++ src/com/gulshansingh/whatsthewifipassword/MainActivity.java
@@ -45,6 +45,11 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		refresh();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
 		displayText();
 	}
 
